27dca6621f836e62dc5d8215eaa9957ad037896c
vtmx <vtmx22@gmail.com>
2021-05-15 18:07:00 -0300

    add some colors

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,30 +1,19 @@
 <template>
   <nav>
-    <div class="logo">
+    <h1 class="logo">
       <a href="#">Vitor Melo</a>
-    </div>
+    </h1>
     <div class="menu">
       <h2 class="invisible">Menu</h2>
       <template v-for="item in nav" :key="item.name">
-        <a
-          :href="item.link"
-          :class="{ active: isActive }"
-          @click="toggleActive($event)"
-          ><i :class="item.icon" class="invisible"></i> {{ item.name }}</a
-        >
+        <a :href="item.link" :class="{ active: isActive }" @click="toggleActive($event)"><i :class="item.icon" class="invisible"></i> {{ item.name }}</a>
       </template>
     </div>
 
     <div class="social">
       <template v-for="social in social" :key="social.name">
-        <a
-          class="btn-icon"
-          :href="social.link"
-          :title="social.name"
-          :class="{ active: isActive }"
-          target="_blank"
-          ><i :class="social.icon"></i>
-          <span class="invisible">{{ social.name }}</span></a
+        <a class="btn-icon" :href="social.link" :title="social.name" :class="{ active: isActive }" target="_blank"
+          ><i :class="social.icon"></i> <span class="invisible">{{ social.name }}</span></a
         >
       </template>
     </div>
@@ -32,11 +21,11 @@
 </template>
 
 <script>
-import nav from "../data/nav.json";
-import social from "../data/social.json";
+import nav from '../data/nav.json';
+import social from '../data/social.json';
 
 export default {
-  name: "NavSocial",
+  name: 'NavSocial',
 
   data() {
     return {
@@ -47,11 +36,11 @@ export default {
   },
   methods: {
     toggleActive(e) {
-      let menuItems = document.querySelectorAll(".menu a");
+      let menuItems = document.querySelectorAll('.menu a');
       menuItems.forEach((item) => {
-        item.classList.remove("active");
+        item.classList.remove('active');
       });
-      e.target.classList.add("active");
+      e.target.classList.add('active');
     },
   },
 };
@@ -82,15 +71,15 @@ nav {
     display: block;
     flex-direction: column;
     padding: 16px 32px;
-    color: var(--grey600);
+    color: var(--menu-fg);
 
     &:hover {
       text-indent: 4px;
-      color: var(--primary);
+      color: var(--menu-fg-hover);
     }
 
     &:active {
-      color: var(--grey800);
+      color: var(--menu-fg-active);
       transition: none;
     }
   }
